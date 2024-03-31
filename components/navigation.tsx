af74ec4c8089fe10230b54913f1339998c0b9b9a
sihyeong <syhy7051@gmail.com>
2024-03-31 23:12:43 +0900

    deploy

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,18 +1,19 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <Link href="/">Home</Link> {path === "/" ? "o" : ""}
+          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
         </li>
         <li>
           <Link href="about-us">About Us</Link>{" "}
-          {path === "/about-us" ? "o" : ""}
+          {path === "/about-us" ? "🔥" : ""}
         </li>
       </ul>
     </nav>
